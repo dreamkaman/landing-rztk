49195883e274fdbe3cc7fa1ecaaae18a60511f53
Dreamkaman <riabushko@gmail.com>
2023-09-19 17:35:41 +0300

    Next

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'index.html'),
+      scriptLoading: 'module', //script attribute
     }),
     new HtmlWebpackPartialsPlugin([
       {
